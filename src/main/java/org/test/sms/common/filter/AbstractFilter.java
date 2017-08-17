c57a2b7e9527e1691a089d0832ad9760d587c1cc
nkvaratskhelia <nkvar12@gmail.com>
2017-08-17 17:56:21 +0300

    added paging parameters to abstract filter

--- src/main/java/org/test/sms/common/filter/AbstractFilter.java
+++ src/main/java/org/test/sms/common/filter/AbstractFilter.java
@@ -1,3 +1,24 @@
 package org.test.sms.common.filter;
 
-public class AbstractFilter {}
+public class AbstractFilter {
+
+    private Long offset;
+
+    private Long numRows;
+
+    public Long getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Long offset) {
+        this.offset = offset;
+    }
+
+    public Long getNumRows() {
+        return numRows;
+    }
+
+    public void setNumRows(Long numRows) {
+        this.numRows = numRows;
+    }
+}
