69d2ae03e1b4cd02cbf1b0157030b84a6d65780e
dmumladze <d.mumladze@ge.ge>
2017-08-17 16:08:22 +0400

    Added logout button. Added Css to Login page. Improved navigation bar

--- src/main/ngapp/src/app/app.component.ts
+++ src/main/ngapp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from "@angular/core";
+import {AuthenticationService} from "./service/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
 	selector: 'app',
@@ -6,4 +8,18 @@ import {Component} from "@angular/core";
 	styleUrls: ['./app.component.css'],
 	providers: []
 })
-export class AppComponent {}
+export class AppComponent {
+
+	constructor(private router: Router) {
+
+	}
+
+	logout (): void{
+		AuthenticationService.logout();
+		this.router.navigate(['login']);
+	}
+
+	isLoggedIn(): boolean {
+		return AuthenticationService.isLoggedIn();
+	}
+}
