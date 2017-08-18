316d15f7f4cf6a56dca5213843ff2a399a9c6d63
nkvaratskhelia <nkvar12@gmail.com>
2017-08-18 17:56:49 +0300

    working on users tab

--- src/main/ngapp/src/app/component/users/users.component.ts
+++ src/main/ngapp/src/app/component/users/users.component.ts
@@ -1,62 +1,30 @@
-import {Component, OnInit} from "@angular/core";
+import {Component} from "@angular/core";
 
 import {User} from "../../model/user";
 import {UserService} from "../../service/user.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {LazyLoadEvent} from "primeng/components/common/lazyloadevent";
 import {UserFilter} from "../../model/filter/user-filter";
+import {AbstractComponent} from "../abstract-component";
+import {Utils} from "../../utils/Utils";
 
 @Component({
 	selector: 'users',
 	templateUrl: './users.component.html',
 	styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent extends AbstractComponent {
 
 	users: User[];
 
-	searchUser: User;
+	totalRecords: number;
 
 	showUserDialog: boolean;
 
 	selectedUser: User = new User();
 
-	totalRecords: number;
-
-	constructor(private router: Router, private route: ActivatedRoute, private userService: UserService) {}
-
-	ngOnInit() {
-		// this.loadUsers();
-	}
-
-	loadUsers(event: LazyLoadEvent): void {
-		let filter: UserFilter = {
-			offset: event.first,
-			numRows: event.rows
-		};
-
-		this.userService.getCount(filter).then(count => this.totalRecords = count, error => {
-			console.error(error);
-		});
-
-		this.userService.getList(filter).then(users => this.users = users, error => {
-			console.error(error);
-		});
-	}
-
-	load(event: LazyLoadEvent) {
-		//event.first = First row offset
-		//event.rows = Number of rows per page
-		//event.sortField = Field name to sort with
-		//event.sortOrder = Sort order as number, 1 for asc and -1 for dec
-		//filters: FilterMetadata object having field as key and filter value, filter matchMode as value
-
-		//imitate db connection over a network
-		// setTimeout(() => {
-		// 	if (this.datasource) {
-		// 		this.cars = this.datasource.slice(event.first, (event.first + event.rows));
-		// 	}
-		// }, 250);
+	constructor(private router: Router, private route: ActivatedRoute, private userService: UserService) {
+		super();
 	}
 
 	add(): void {}
@@ -65,8 +33,19 @@ export class UsersComponent implements OnInit {
 
 	delete(): void {}
 
-	selectItem(user: User) {
+	get(user: User) {
 		this.selectedUser = user;
 		this.showUserDialog = true;
+	}
+
+	getList(event: LazyLoadEvent): void {
+		console.log(event.filters);
+		let filter: UserFilter = {
+			offset: event.first,
+			numRows: event.rows
+		};
+
+		this.userService.getCount(filter).then(count => this.totalRecords = count, error => Utils.handleError(error));
+		this.userService.getList(filter).then(users => this.users = users, error => Utils.handleError(error));
 	}
 }
---
+++ src/main/ngapp/src/app/utils/Utils.ts
@@ -0,0 +1,8 @@
+export class Utils {
+
+	private Utils() {}
+
+	static handleError(error: any) {
+		console.error(error);
+	}
+}
