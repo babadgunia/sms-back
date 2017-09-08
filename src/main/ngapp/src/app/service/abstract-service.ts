4ab43b185c2d8a71271d7fee37c8742fe1bbdc30
nkvaratskhelia <nkvar12@gmail.com>
2017-09-08 11:09:00 +0300

    updated angular-cli version

--- src/main/ngapp/src/app/service/abstract-service.ts
+++ src/main/ngapp/src/app/service/abstract-service.ts
@@ -7,6 +7,7 @@ import {Utils} from "../util/utils";
 import {Observable} from "rxjs/Observable";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
+import 'rxjs/add/observable/throw';
 
 export abstract class AbstractService {
 
