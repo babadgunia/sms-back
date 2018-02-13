69281a43a2d9b959942c2e48533ae38bd8b60331
David Mumladze - HELMES <dmuml10@gmail.com>
2018-02-13 11:37:02 +0200

    Added new cors filter

---
+++ sms-back/src/main/java/org/test/sms/web/misc/CustomCorsFilter.java
@@ -0,0 +1,28 @@
+package org.test.sms.web.misc;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomCorsFilter extends CorsFilter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+
+        chain.doFilter(req, res);
+    }
+
+}
