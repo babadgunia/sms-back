4e064805851021a5e273f953ed7fa2674e230dbc
david.mumladze <dmuml10@gmail.com>
2018-11-13 15:21:21 +0200

    Added UserDao Integration Test

---
+++ src/test/java/org/test/sms/server/dao/general/TestUserDao.java
@@ -0,0 +1,51 @@
+package org.test.sms.server.dao.general;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.test.sms.common.entity.general.User;
+import org.test.sms.common.entity.general.UserGroup;
+import org.test.sms.server.dao.impl.general.UserDaoImpl;
+import org.test.sms.server.dao.interfaces.general.UserDao;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ComponentScan(basePackages = {"org.test.sms"})
+public class TestUserDao {
+
+  @Autowired
+  private TestEntityManager entityManager;
+
+  @Autowired
+  private UserDaoImpl userDao;
+
+  @Test
+  public void whenUsernameExists_thenReturnTrue() {
+    User u = new User();
+    u.setUsername("Test");
+    u.setCreated(ZonedDateTime.now());
+    u.setLastModified(ZonedDateTime.now());
+
+    UserGroup ug = new UserGroup();
+    ug.setCreated(ZonedDateTime.now());
+    ug.setLastModified(ZonedDateTime.now());
+    entityManager.persist(ug);
+    entityManager.flush();
+    u.setUserGroup(ug);
+
+    entityManager.persist(u);
+    entityManager.flush();
+
+    assertTrue(userDao.exists(u.getUsername()));
+  }
+
+}
