6163bbedfab923ccb9254873a5d70a4e13ea66a5
David <dmuml10@gmail.com>
2021-10-12 10:09:05 +0300

    Update maven dependencies; Fix unit tests

--- src/main/java/org/test/sms/web/dto/general/UserDto.java
+++ src/main/java/org/test/sms/web/dto/general/UserDto.java
@@ -5,9 +5,6 @@ import lombok.Setter;
 import org.test.sms.common.enums.general.LanguageType;
 import org.test.sms.common.enums.general.StatusType;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @Getter @Setter
 public class UserDto {
 
@@ -15,21 +12,15 @@ public class UserDto {
 
     private long id;
 
-    @NotBlank
     private String username;
 
-    @NotBlank
     private String email;
 
-    @NotBlank
     private String name;
 
-    @NotNull
     private StatusType status;
 
-    @NotNull
     private LanguageType language;
 
-    @NotNull
     private UserGroupDto userGroup;
 }
